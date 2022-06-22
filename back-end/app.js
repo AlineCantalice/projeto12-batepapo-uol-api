5d678791353c807188e84b26f82d8595a212298f
AlineCantalice <alinedecantalice@gmail.com>
2022-06-22 17:07:06 -0300

    configurações iniciais

---
+++ back-end/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000);
