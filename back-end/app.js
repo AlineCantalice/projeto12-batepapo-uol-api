44af0b6d5ec9e6b2477fa3a33aea2ac5a725cd0c
AlineCantalice <alinedecantalice@gmail.com>
2022-06-22 19:39:30 -0300

    get e post de participants - falta ajustes

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import joi from 'joi';
 import { MongoClient } from 'mongodb';
 
 dotenv.config();
@@ -8,5 +9,43 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+let db;
+const client = new MongoClient(process.env.MONGO_URI);
+const promise = client.connect().then(() => db = client.db('bate_papo'));
+
+const schema = joi.object().keys({
+    name: joi.string().required()
+});
+
+app.post('/participants', (req, res) => {
+    const { name } = req.body;
+    const { error, value } = schema.validate({ name: name });
+
+    if (!error) {
+        db.collection('participants')
+            .insertOne({
+                name: name,
+                lastStatus: Date.now()
+            }).then(res.sendStatus(201));
+        return;
+    }
+    return res.sendStatus(422);
+});
+
+app.get('/participants', (req, res) => {
+    db.collection('participants')
+        .find().toArray().then(participants => {
+            res.send(participants)
+        })
+});
+
+app.post('/messages', (req, res) => {
+
+});
+
+app.get('/messages', (req, res) => {
+    
+});
 
 app.listen(5000);
