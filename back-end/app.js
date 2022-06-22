212ae397874090e8b6c9188d23a14ac4b6c74ca6
AlineCantalice <alinedecantalice@gmail.com>
2022-06-22 20:15:13 -0300

    get e post das mensagens - falta ajustes

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import joi from 'joi';
+import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
 
 dotenv.config();
@@ -14,12 +15,13 @@ let db;
 const client = new MongoClient(process.env.MONGO_URI);
 const promise = client.connect().then(() => db = client.db('bate_papo'));
 
-const schema = joi.object().keys({
-    name: joi.string().required()
-});
-
 app.post('/participants', (req, res) => {
     const { name } = req.body;
+
+    const schema = joi.object().keys({
+        name: joi.string().required()
+    });
+
     const { error, value } = schema.validate({ name: name });
 
     if (!error) {
@@ -41,11 +43,41 @@ app.get('/participants', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
+    const { to, text, type } = req.body;
+    const from = req.headers.user;
+    const time = dayjs().format('HH:mm:ss')
 
+    const schema = joi.object().keys({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message'),
+        from: joi.string().valid(from)
+    });
+
+    const { error, value } = schema.validate({ to: to, text: text, type: type, from: from });
+
+    if (!error) {
+        db.collection('messages')
+            .insertOne({
+                from: from,
+                to: to,
+                text: text,
+                type: type,
+                time: time
+            }).then(res.sendStatus(201));
+        return;
+    }
+    return res.sendStatus(422);
 });
 
 app.get('/messages', (req, res) => {
-    
+    const limit = req.query;
+    const user = req.headers.user;
+    db.collection('messages')
+        .find().toArray().then(messages => {
+            console.log(messages)
+            res.send(messages)
+        })
 });
 
 app.listen(5000);
